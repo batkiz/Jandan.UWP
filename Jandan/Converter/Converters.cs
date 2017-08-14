b0cdb83bfba1ea19c154ac99f9ba258d4734f4d6
Ray Lee <raysworld@outlook.com>
2017-08-15 00:28:16 +0800

    修复由于图片不存在造成的闪退bug

--- Jandan/Converter/Converters.cs
+++ Jandan/Converter/Converters.cs
@@ -1,11 +1,14 @@
-﻿using Jandan.UWP.Core.Models;
+﻿using Jandan.UWP.Core.HTTP;
+using Jandan.UWP.Core.Models;
 using Jandan.UWP.Core.ViewModels;
+using Microsoft.Toolkit.Uwp.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace Jandan.UWP.UI
 {
@@ -231,4 +234,23 @@ namespace Jandan.UWP.UI
             throw new NotImplementedException();
         }
     }
+
+    public class UrlToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (string.IsNullOrEmpty(value as string))
+            {
+                return null;
+            }
+
+            else return new BitmapImage(new Uri(value as string, UriKind.Absolute));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
